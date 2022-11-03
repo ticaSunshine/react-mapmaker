f2840af8927453b64f88c8731beeb1049b517814
Tica Sunshine <vranic.tica@gmail.com>
2022-11-03 20:26:37 +0100

    test web completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../App.css";
 import Cards from "../Cards";
-import Footer from "../Footer";
 import HeroSection from "../HeroSection";
 
 function Home() {
@@ -9,7 +8,6 @@ function Home() {
     <>
       <HeroSection />
       <Cards />
-      <Footer />
     </>
   );
 }
