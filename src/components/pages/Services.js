5e0cadba7fc7a7ed9271fbc8e8cf3b5b099e961d
Tica Sunshine <vranic.tica@gmail.com>
2022-11-05 20:00:02 +0100

    import leaflet

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,27 @@
 import React from "react";
+import "leaflet/dist/leaflet.css";
 import "../../App.css";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 
 export default function Services() {
-  return <h1 className="services">SERVICES</h1>;
+  return (
+    <>
+      <MapContainer
+        className="services"
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
 }
