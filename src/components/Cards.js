6b4350520cdf99cf8227962776742e4bbb9f07a4
Tica Sunshine <vranic.tica@gmail.com>
2022-11-05 19:04:49 +0100

    app init

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ import "./Cards.css";
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out these EPIC Destination</h1>
+      <h1>Check out these EPIC Dest</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
