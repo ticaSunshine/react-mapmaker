8cffbf578e7c305f586fb535088229363a69417f
Tica Sunshine <vranic.tica@gmail.com>
2022-11-06 17:24:56 +0100

    working on SpotContainer

---
+++ src/MapComponents/SpotsContainer.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Marker, Popup } from "react-leaflet";
+
+// Spot
+
+let Spots = [
+  {
+    Name: "Mimara",
+    Description: "7 stairs",
+    Address: "Rooseveltov trg 5, 10000, Zagreb",
+    Coordinates: [45.80817, 15.967346],
+    Tags: "Stairs, DIY, Ledges ",
+    Fotke: [
+      "/0_helpers/Spotovi/Mimara/0.JPG",
+      "/0_helpers/Spotovi/Mimara/1.JPG",
+      "/0_helpers/Spotovi/Mimara/2.JPG",
+      "/0_helpers/Spotovi/Mimara/3.JPG",
+      "/0_helpers/Spotovi/Mimara/4.JPG",
+      "/0_helpers/Spotovi/Mimara/5.JPG",
+      "/0_helpers/Spotovi/Mimara/5.JPG"
+    ]
+  },
+  {
+    Name: "Đamija",
+    Description: "Ledges and stairs",
+    Address: "Trg žrtava fašizma, 10000, Zagreb",
+    Coordinates: [45.8102, 15.9867],
+    Tags: "Ledges, Stairs, Gaps ",
+    Fotke: [
+      "/0_helpers/Spotovi/Đamija/0.JPG",
+      "/0_helpers/Spotovi/Đamija/1.JPG",
+      "/0_helpers/Spotovi/Đamija/2.JPG",
+      "/0_helpers/Spotovi/Đamija/3.JPG",
+      "/0_helpers/Spotovi/Đamija/4.JPG",
+      "/0_helpers/Spotovi/Đamija/5.JPG",
+      "/0_helpers/Spotovi/Đamija/5.JPG"
+    ]
+  },
+  {
+    Name: "h",
+    Description: "Lairs",
+    Address: "Trg žrtava fašizma, 10000, Zagreb",
+    Coordinates: [45.81, 15.98],
+    Tags: "Ledges, Stairs, Gaps ",
+    Fotke: [
+      "/0_helpers/Spotovi/Đamija/0.JPG",
+      "/0_helpers/Spotovi/Đamija/1.JPG",
+      "/0_helpers/Spotovi/Đamija/2.JPG",
+      "/0_helpers/Spotovi/Đamija/3.JPG",
+      "/0_helpers/Spotovi/Đamija/4.JPG",
+      "/0_helpers/Spotovi/Đamija/5.JPG",
+      "/0_helpers/Spotovi/Đamija/5.JPG"
+    ]
+  }
+];
+
+export default function SpotsContainer() {
+  return Spots.map((spot, index) => {
+    return (
+      <div key={index}>
+        <Marker position={spot.Coordinates}>
+          <Popup>{spot.Name}</Popup>
+        </Marker>
+      </div>
+    );
+  });
+}
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,14 +1,40 @@
 import React from "react";
+import { useState } from "react";
 import "leaflet/dist/leaflet.css";
 import "../../App.css";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  useMapEvents,
+  Marker,
+  Popup
+} from "react-leaflet";
+import SpotsContainer from "../../MapComponents/SpotsContainer";
 
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    }
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
 export default function Services() {
   return (
     <>
       <MapContainer
         className="services"
-        center={[51.505, -0.09]}
+        center={{ lat: 41.505, lng: 2.17 }}
         zoom={13}
         scrollWheelZoom={false}
       >
@@ -16,11 +42,8 @@ export default function Services() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <LocationMarker />
+        <SpotsContainer />
       </MapContainer>
     </>
   );
