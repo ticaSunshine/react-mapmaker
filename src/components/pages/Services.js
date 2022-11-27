5c08e7d9cae3f35f87654657a0fab00874cd84e4
Tica Sunshine <vranic.tica@gmail.com>
2022-11-27 18:33:01 +0100

    fullscreen btn finish

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,35 +2,40 @@ import React from "react";
 import { useState } from "react";
 import "leaflet/dist/leaflet.css";
 import "../../App.css";
-import "../MapComponents/MapContainer.css";
+import "../MapComponents/LeafletContainer.css";
 import { MapContainer, TileLayer } from "react-leaflet";
 import SpotsContainer from "../MapComponents/SpotsContainer.js";
 import LocationMarker from "../MapComponents/LocationMarker";
 import FullscreenBtn from "../MapComponents/FullscreenBtn";
-import { map } from "leaflet";
 
 export default function Services() {
   const [fullScreen, setFullScreen] = useState(false);
   return (
     <div className="services">
-      <MapContainer
-        className={() =>
-          ///zapeo na full screen modu
+      <div
+        className={
           "leaflet-container " +
-          (fullScreen ? "leaflet-container--fullScrn" : "")
+          (!fullScreen ? "leaflet-container--fullScrn" : "")
         }
-        center={{ lat: 41.505, lng: 2.17 }}
-        zoom={13}
-        scrollWheelZoom={false}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <FullscreenBtn fullScreen={fullScreen} setFullScreen={setFullScreen} />
-        <LocationMarker />
-        <SpotsContainer />
-      </MapContainer>
+        <MapContainer
+          className="map-container"
+          center={{ lat: 45.46611, lng: 16.37833 }}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <FullscreenBtn
+            fullScreen={fullScreen}
+            setFullScreen={setFullScreen}
+          />
+          <LocationMarker />
+          <SpotsContainer />
+        </MapContainer>
+      </div>
     </div>
   );
 }
